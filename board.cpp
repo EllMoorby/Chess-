9385d7b753151d28cf74ce455fb591cc192750d1
NightMeh <99148447+NightMeh@users.noreply.github.com>
2023-03-08 10:52:23 +0000

    Fixed bugs. Added exporting of FEN string

--- board.cpp
+++ board.cpp
@@ -8,6 +8,7 @@ Board::Board(){
 }
 
 void Board::loadFEN(std::string fen) {
+	//REQUIRES A SPACE AT END OF FEN
 
 	Pieces board[64] = {
 		empty, empty, empty, empty, empty, empty, empty, empty,
@@ -25,6 +26,7 @@ void Board::loadFEN(std::string fen) {
 	int start = 0;
 	int end = fen.find(" ");
 	int currentField = 0;
+	
 	while (end != -1) {
 		fenFields[currentField] = fen.substr(start, end - start);
 		start = end + 1;
@@ -33,10 +35,10 @@ void Board::loadFEN(std::string fen) {
 	}
 
 	unsigned int currentIndex = 0;
+
 	for (auto& piecePlacementData : fenFields[0]) {
 		switch (piecePlacementData) {
 			case '/':
-				currentIndex++;
 				break;
 			case '1':
 				currentIndex += 1;
@@ -63,57 +65,60 @@ void Board::loadFEN(std::string fen) {
 				currentIndex += 8;
 				break;
 			case 'k':
-				board[currentIndex] = blackKing;
+				this->board[currentIndex] = blackKing;
 				currentIndex++;
 				break;
 			case 'q':
-				board[currentIndex] = blackQueen;
+				this->board[currentIndex] = blackQueen;
 				currentIndex++;
 				break;
 			case 'r':
-				board[currentIndex] = blackRook;
+				this->board[currentIndex] = blackRook;
 				currentIndex++;
 				break;
 			case 'b':
-				board[currentIndex] = blackBishop;
+				this->board[currentIndex] = blackBishop;
 				currentIndex++;
 				break;
 			case 'n':
-				board[currentIndex] = blackKnight;
+				this->board[currentIndex] = blackKnight;
 				currentIndex++;
 				break;
 			case 'p':
-				board[currentIndex] = blackPawn;
+				this->board[currentIndex] = blackPawn;
 				currentIndex++;
 				break;
 			case 'K':
-				board[currentIndex] = whiteKing;
+				this->board[currentIndex] = whiteKing;
 				currentIndex++;
 				break;
 			case 'Q':
-				board[currentIndex] = whiteQueen;
+				this->board[currentIndex] = whiteQueen;
 				currentIndex++;
 				break;
 			case 'R':
-				board[currentIndex] = whiteRook;
+				this->board[currentIndex] = whiteRook;
 				currentIndex++;
 				break;
 			case 'B':
-				board[currentIndex] = whiteBishop;
+				this->board[currentIndex] = whiteBishop;
 				currentIndex++;
 				break;
 			case 'N':
-				board[currentIndex] = whiteKnight;
+				this->board[currentIndex] = whiteKnight;
+				std::cout << this->board[currentIndex] << std::endl;
 				currentIndex++;
 				break;
 			case 'P':
-				board[currentIndex] = whitePawn;
+				this->board[currentIndex] = whitePawn;
 				currentIndex++;
 				break;
 
 
 		}
 	}
+
+	
 
 	activeColour = (fenFields[1] == "w")? white : black;
 	castlingAvailability.whiteKingSide = (fenFields[2][0] != '-') ? true : false;
@@ -122,15 +127,96 @@ void Board::loadFEN(std::string fen) {
 	castlingAvailability.blackQueenSide = (fenFields[2][3] != '-') ? true : false;
 
 	enpassant64Index = algebraicNotationTo64Index(fenFields[3]);
-	//fucking hell causese error
+
 	halfmoveClock = std::stoi(fenFields[4]);
 	fullmoveNumber = std::stoi(fenFields[5]);
-
 
 }
 
 std::string Board::getBoardAsFEN() {
-	return "a";
+	std::string fen = "";
+	unsigned int count = 0;
+	unsigned int rowtotal = 0;
+	for (auto& piecePlacementData : board) {
+		if (rowtotal == 8) {
+			rowtotal = 0;
+			if (count > 0) {
+				fen.append(std::to_string(count));
+			}
+			count = 0;
+			fen.append("/");
+		}
+		if ((piecePlacementData != empty) && (count > 0)){
+			fen.append(std::to_string(count));
+			count = 0;
+		}
+		
+		switch (piecePlacementData){
+			case blackKing:
+				rowtotal++;
+				fen.append("k");
+				break;
+			case blackQueen:
+				rowtotal++;
+				fen.append("q");
+				break;
+			case blackRook:
+				rowtotal++;
+				fen.append("r");
+				break;
+			case blackBishop:
+				rowtotal++;
+				fen.append("b");
+				break;
+			case blackKnight:
+				rowtotal++;
+				fen.append("n");
+				break;
+			case blackPawn:
+				rowtotal++;
+				fen.append("p");
+				break;
+			case whiteKing:
+				rowtotal++;
+				fen.append("K");
+				break;
+			case whiteQueen:
+				rowtotal++;
+				fen.append("Q");
+				break;
+			case whiteRook:
+				rowtotal++;
+				fen.append("R");
+				break;
+			case whiteBishop:
+				rowtotal++;
+				fen.append("B");
+				break;
+			case whiteKnight:
+				rowtotal++;
+				fen.append("N");
+				break;
+			case whitePawn:
+				rowtotal++;
+				fen.append("P");
+				break;
+			case empty:
+				if (count == 8) {
+					fen.append(std::to_string(count));
+					count = 0;
+					rowtotal = 0;
+				}
+				count++;
+				rowtotal++;
+		}
+	}
+	std::string fenActiveColour = (activeColour == black) ? "b" : "w";
+	fen.append(fenActiveColour);
+	//Add castling rights to fen
+	//Add lastmove to fen
+	//Add halfmove clock
+	//Add fullmove number
+	return fen;
 }
 
 unsigned int Board::algebraicNotationTo64Index(std::string square) {
@@ -142,6 +228,6 @@ unsigned int Board::algebraicNotationTo64Index(std::string square) {
 void Board::displayBoard() {
 	for (auto& piecePlacementData : board) {
 		std::cout << piecePlacementData;
-
 	}
+	std::cout << std::endl << enpassant64Index << std::endl << halfmoveClock << std::endl<< fullmoveNumber;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include "board.h"
+#include <iostream>
 
 int main()
 {
     Board board;
-    board.loadFEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
-    board.displayBoard();
+    board.loadFEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 32 1 ");
+    std::string test = board.getBoardAsFEN();
     return 0;
 }
