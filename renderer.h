227e0e990178286a9c7cf010cfaec432321ac127
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2023-03-08 16:56:09 +0000

    Update renderer.h

--- renderer.h
+++ renderer.h
@@ -1,5 +1,39 @@
 #pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+struct RendererConstructorArgs {
+	unsigned int screenHeight;
+	unsigned int screenWidth;
+	std::string texturePath;
+};
+
 class Renderer
 {
+private:
+	unsigned int screenHeight;
+	unsigned int screenWidth;
+	sf::Window window;
+	
+	sf::Texture blackKing;
+	sf::Texture blackQueen;
+	sf::Texture blackRook;
+	sf::Texture blackBishop;
+	sf::Texture blackKnight;
+	sf::Texture blackPawn;
+	sf::Texture whiteKing;
+	sf::Texture whiteQueen;
+	sf::Texture whiteRook;
+	sf::Texture whiteBishop;
+	sf::Texture whiteKnight;
+	sf::Texture whitePawn;
+
+public:
+	Renderer(RendererConstructorArgs& args);
+	void createWindow();
+	void destroyWindow();
+	void draw(unsigned int x, unsigned int y, sf::Sprite sprite);
+
 };
 
